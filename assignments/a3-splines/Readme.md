a101e28ac5726f986bb6b2c8b11fdb24dbd296b2
akmazzoli <akmazzoli@gmail.com>
2021-10-01 20:55:49 -0400

    assignment 3

--- assignments/a3-splines/Readme.md
+++ assignments/a3-splines/Readme.md
@@ -1,7 +1,3 @@
 # Readme
 
-TODO: Replace this text with
-
-* images, gifs, videos
-* brief description of what each image/gif/video shows
 
--- assignments/a3-splines/interpolator_hermite.h
+++ assignments/a3-splines/interpolator_hermite.h
@@ -16,15 +16,22 @@ public:
     InterpolatorHermite() : Interpolator("Hermite"), mClampDir(glm::vec3(1,0,0)) {}
 
     virtual glm::vec3 interpolate(int segment, double u) const {
-       // todo: your code here
-       return glm::vec3(0);
+       //2 points and 2 slopes
+       //b0,b3 slopes, p10,p11 points?
+       vec3 b0;
+       vec3 b3;
+       glm::vec3 next = b0*(1-3(u*u)+2(u*u*u)) + mCtrlPoints[segment]*(u*(1-u)*(1-u)) + mCtrlPoints[segment+1]((u*u)+(u*u*u)) + b3(3(u*u)-2(u*u*u));
+       return next;
     }
 
     virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
-       // todo: your code here
+       for(int i = 2; i < keys.size(); i = i++){
+           mCtrlPoints.push_back(3(keys[i]-keys[i-2]));
+       }
     }
 
     void setClamped(bool c) { mIsClamped = c; }
+    //clamp end cases
     bool isClamped() const { return mIsClamped; }
 
     void setClampedDirection(const glm::vec3& v) { mClampDir = v; }
